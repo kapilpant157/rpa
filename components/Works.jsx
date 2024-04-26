a10ae0f4840d3b9bc51f29807c427a0ec0b10b2d
kapil <kapilpant157@gmail.com>
2024-04-26 15:53:27 +0530

    works images

--- components/Works.jsx
+++ components/Works.jsx
@@ -46,14 +46,14 @@ const Works = () => {
                         <div class="flex relative">
                             <Image
                                 className="absolute inset-0 w-full h-full object-cover object-center"
-                                src="/healthrightinternational.png"
+                                src="/kitab.jpg"
                                 width={600}
                                 height={360}
                                 alt="Health Right International"
                             />
                             <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-                                <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">THE SUBTITLE</h2>
-                                <h1 class="title-font text-lg font-medium text-gray-900 mb-3">The 400 Blows</h1>
+                                <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">Internal Audit</h2>
+                                <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Kanun Kitaab Bywastha Samiti</h1>
                                 <p class="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
                             </div>
                         </div>
@@ -62,14 +62,14 @@ const Works = () => {
                         <div class="flex relative">
                             <Image
                                 className="absolute inset-0 w-full h-full object-cover object-center"
-                                src="/healthrightinternational.png"
+                                src="/nathm.jpg"
                                 width={600}
                                 height={360}
                                 alt="Health Right International"
                             />
                             <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-                                <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">THE SUBTITLE</h2>
-                                <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Neptune</h1>
+                                <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">Internal Audit</h2>
+                                <h1 class="title-font text-lg font-medium text-gray-900 mb-3">NATHAM</h1>
                                 <p class="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
                             </div>
                         </div>
@@ -78,14 +78,14 @@ const Works = () => {
                         <div class="flex relative">
                             <Image
                                 className="absolute inset-0 w-full h-full object-cover object-center"
-                                src="/healthrightinternational.png"
+                                src="/etech.jpg"
                                 width={600}
                                 height={360}
                                 alt="Health Right International"
                             />
                             <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-                                <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">THE SUBTITLE</h2>
-                                <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Holden Caulfield</h1>
+                                <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">Staturory Audit</h2>
+                                <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Etech International P. Ltd.</h1>
                                 <p class="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
                             </div>
                         </div>
@@ -94,14 +94,14 @@ const Works = () => {
                         <div class="flex relative">
                             <Image
                                 className="absolute inset-0 w-full h-full object-cover object-center"
-                                src="/healthrightinternational.png"
+                                src="/neaeng.jpg"
                                 width={600}
                                 height={360}
-                                alt="Health Right International"
+                                alt="NEA Engineering Company Ltd"
                             />
                             <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-                                <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">THE SUBTITLE</h2>
-                                <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Alper Kamu</h1>
+                                <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">Internal Audit</h2>
+                                <h1 class="title-font text-lg font-medium text-gray-900 mb-3">NEA Engineering Company Ltd.</h1>
                                 <p class="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
                             </div>
                         </div>
