904dcf198bf5119be746271e00d04c5e3ce46f80
kapil <kapilpant157@gmail.com>
2024-04-26 14:17:54 +0530

    build successfully

--- components/MainLayout.jsx
+++ components/MainLayout.jsx
@@ -3,7 +3,7 @@ import Navbar from '@/components/Navbar'
 import Footer from './Footer'
 
 import Blogs from './Blogs'
-import ContactUs from './ContactUs'
+
 
 const MainLayout = ({ children }) => {
     return (
