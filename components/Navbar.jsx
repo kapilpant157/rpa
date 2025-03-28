2b9f2faaadc5a4fd8eb577afa9f6a2ea35e676d6
kapil <kapilpant157@gmail.com>
2025-03-29 01:00:19 +0545

    logo

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,8 +6,15 @@ const Navbar = () => {
   return (
     <header className="text-gray-600 body-font border-2px-solid-red sticky top-0 w-full bg-white z-10">
       <div className="container mx-auto flex flex-wrap p-4 flex-col md:flex-row items-center">
-        <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-          <span className="ml-3 text-xl">caravindra@outlook.com</span>
+        <a className="flex title-font font-medium items-center text-gray-900  md:mb-0">
+          {/* <span className="ml-3 text-xl">caravindra@outlook.com</span> */}
+          <Image
+                className="w-48 h-16  flex-shrink-0 object-cover object-center "
+                src="/logo.jpg"
+                width={720}
+                height={600}
+                alt="logo"
+              />
         </a>
         <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
           <Link className="ml-5 hover:text-gray-900" href="/">
